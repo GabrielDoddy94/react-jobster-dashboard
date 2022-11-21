5729ef7c79377c47c5912c099e4abc52934b8031
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-21 12:09:34 -0300

    react router - setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Dashboard, Error, Landing, Register } from "./pages";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import main from "../assets/images/main.svg";
 
 import Wrapper from "../assets/wrappers/LandingPage";
@@ -20,7 +22,9 @@ const Landing = () => {
             meggings poutine sriracha four loko distillery mlkshk put a bird on
             it. Tacos kickstarter actually ethical cornhole.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
 
         <img src={main} alt="job hunt" className="img main-img" />
