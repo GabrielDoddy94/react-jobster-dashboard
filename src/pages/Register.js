b3df0aa45c4e8e0c4b0b407cd582e727410302ea
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-22 18:09:54 -0300

    handle change and empty values functionality

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -14,12 +14,18 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
 
   const handleChange = e => {
-    console.log(e.target);
+    const { name, value } = e.target;
+
+    setValues({ ...values, [name]: value });
   };
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+
+    if (!email || !password || (!isMember && !name)) {
+      console.log("Please fill out all fields");
+    }
   };
 
   const toggleMember = () => {
