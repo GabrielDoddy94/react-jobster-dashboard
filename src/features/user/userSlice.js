338a4fd88b5930128b6fc04c7e96b0d0e8a2442c
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-22 20:12:00 -0300

    register user - HTTP(AJAX) Request

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -11,11 +11,10 @@ export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
     try {
-      const resp = await customFetch.post("/auth/testingRegister", user);
-      console.log(resp);
+      const resp = await customFetch.post("/auth/register", user);
+      return resp.data;
     } catch (error) {
-      toast.error(error.response.data.msg);
-      console.log(error.response);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
 );
@@ -30,6 +29,22 @@ export const loginUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(registerUser.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Hello There ${user.name}`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;
