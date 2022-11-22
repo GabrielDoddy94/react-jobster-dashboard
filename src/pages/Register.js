7fa8602a7a854ea483a72a6b940c70eb57981faf
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-22 17:31:51 -0300

    register page - setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
+
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <h1>Register Page</h1>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = e => {
+    console.log(e.target);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            className="form-input"
+            value={values.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
