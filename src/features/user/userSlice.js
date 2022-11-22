358a1a595fb2af2ac98c646aacbb30b59f31816d
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-22 20:17:36 -0300

    login user - HTTP(AJAX) Request

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,7 +22,12 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
-    console.log(`Login User: ${JSON.stringify(user)}`);
+    try {
+      const resp = await customFetch.post("/auth/login", user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
@@ -41,6 +46,19 @@ const userSlice = createSlice({
         toast.success(`Hello There ${user.name}`);
       })
       .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(loginUser.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Welcome Back ${user.name}`);
+      })
+      .addCase(loginUser.rejected, (state, { payload }) => {
         state.isLoading = false;
         toast.error(payload);
       });
