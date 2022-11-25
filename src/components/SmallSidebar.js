cc1fc7024982f0cbcfb52f2f29658c60210686cc
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-24 22:26:18 -0300

    SmallSidebar - nav links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { FaTimes } from "react-icons/fa";
 import { toggleSidebar } from "../features/user/userSlice";
+import links from "../utils/links";
 
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { Logo } from "./";
@@ -28,7 +29,26 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <div className="nav-links">
+            {links.map(link => {
+              const { text, path, id, icon } = link;
+
+              return (
+                <NavLink
+                  key={id}
+                  to={path}
+                  className={({ isActive }) => {
+                    return isActive ? "nav-link active" : "nav-link";
+                  }}
+                  onClick={toggle}
+                  end
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
--- src/utils/links.js
+++ src/utils/links.js
@@ -5,9 +5,9 @@ import { ImProfile } from "react-icons/im";
 
 const links = [
   { id: 1, text: "stats", path: "/", icon: <IoBarChartSharp /> },
-  { id: 2, text: "all jobs", path: "/all-jobs", icon: <MdQueryStats /> },
-  { id: 3, text: "add job", path: "/add-job", icon: <FaWpforms /> },
-  { id: 4, text: "profile", path: "/profile", icon: <ImProfile /> },
+  { id: 2, text: "all jobs", path: "all-jobs", icon: <MdQueryStats /> },
+  { id: 3, text: "add job", path: "add-job", icon: <FaWpforms /> },
+  { id: 4, text: "profile", path: "profile", icon: <ImProfile /> },
 ];
 
 export default links;
