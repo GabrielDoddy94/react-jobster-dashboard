5830bcb45d13ce24239b51fd4ff6a36a73abd2ac
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-24 22:32:18 -0300

    NavLinks component

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,11 +1,9 @@
-import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { FaTimes } from "react-icons/fa";
 import { toggleSidebar } from "../features/user/userSlice";
-import links from "../utils/links";
 
 import Wrapper from "../assets/wrappers/SmallSidebar";
-import { Logo } from "./";
+import { Logo, NavLinks } from "./";
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector(store => store.user);
@@ -29,26 +27,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map(link => {
-              const { text, path, id, icon } = link;
-
-              return (
-                <NavLink
-                  key={id}
-                  to={path}
-                  className={({ isActive }) => {
-                    return isActive ? "nav-link active" : "nav-link";
-                  }}
-                  onClick={toggle}
-                  end
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import BigSidebar from "./BigSidebar";
 import FormRow from "./FormRow";
 import Navbar from "./Navbar";
+import NavLinks from "./NavLinks";
 import Logo from "./Logo";
 import SmallSidebar from "./SmallSidebar";
 
-export { BigSidebar, FormRow, Logo, Navbar, SmallSidebar };
+export { BigSidebar, FormRow, Logo, Navbar, NavLinks, SmallSidebar };
