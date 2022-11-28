47a203385a6f1f822b85adba1e599ee2aebdd0bf
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-27 21:59:51 -0300

    refactor user slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,6 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import customFetch from "../../utils/axios";
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from "./userThunk";
 import {
   addUserToLocalStorage,
   getUserFromLocalStorage,
@@ -15,48 +19,17 @@ const initialState = {
 
 export const registerUser = createAsyncThunk(
   "user/registerUser",
-  async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post("/auth/register", user);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
-  }
+  async (user, thunkAPI) => registerUserThunk("auth/register", user, thunkAPI)
 );
 
 export const loginUser = createAsyncThunk(
   "user/loginUser",
-  async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post("/auth/login", user);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
-  }
+  async (user, thunkAPI) => loginUserThunk("/auth/login", user, thunkAPI)
 );
 
 export const updateUser = createAsyncThunk(
   "user/updateUser",
-  async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.patch("/auth/updateUser", user, {
-        headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-        },
-      });
-
-      return resp.data;
-    } catch (error) {
-      if (error.response.status === 401) {
-        thunkAPI.dispatch(logoutUser());
-        return thunkAPI.rejectWithValue("Unauthorized! Logging Out...");
-      }
-
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
-  }
+  async (user, thunkAPI) => updateUserThunk("/auth/updateUser", user, thunkAPI)
 );
 
 const userSlice = createSlice({
