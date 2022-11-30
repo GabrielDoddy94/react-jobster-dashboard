7050b82edfd326cb0856576219a57bbea0000340
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-11-30 14:51:27 -0300

    get all jobs request

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 import Wrapper from "../assets/wrappers/JobsContainer";
 import Job from "../components/Job";
@@ -8,6 +9,10 @@ import Loading from "./Loading";
 const JobsContainer = () => {
   const { jobs, isLoading } = useSelector(store => store.allJobs);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, [dispatch]);
 
   if (isLoading) {
     return <Loading />;
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -21,9 +21,43 @@ const initialState = {
   ...initialFiltersState,
 };
 
+export const getAllJobs = createAsyncThunk(
+  "allJobs/getJobs",
+  async (_, thunkAPI) => {
+    let url = "/jobs";
+
+    try {
+      const resp = await customFetch.get(url, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      console.log(resp.data);
+
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("There was an error");
+    }
+  }
+);
+
 const allJobsSlice = createSlice({
   name: "allJobs",
   initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(getAllJobs.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getAllJobs.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.jobs = payload.jobs;
+      })
+      .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default allJobsSlice.reducer;
