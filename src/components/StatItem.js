e5eac0c7ca3a0c596d28c627c95894dccd1aaebd
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-12-03 19:47:39 -0300

    StatItem component structure

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,5 +1,15 @@
-const StatItem = () => {
-  return <h2>StatItem</h2>;
+import Wrapper from "../assets/wrappers/StatItem";
+
+const StatItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
 };
 
 export default StatItem;
